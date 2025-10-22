08203dd2a6c309f86e1870e6fc2a6ab484a66578
Brzana <szymonbrzana@gmail.com>
2025-10-22 15:30:56 +0200

    making page in blog/[slug] look somewhat good

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,6 +1,19 @@
 "use client";
 
+import { blogPosts } from "@/app/_data/blogPosts";
+
 export default function Page({ params }: { params: { slug: string } }) {
     const { slug } = params;
-    return <>Blog Post Slug: {slug}</>;
+    const post = blogPosts.find((post) => post.slug === slug);
+
+    if (!post) {
+        return <>Blog Post Not Found</>;
+    }
+
+    return (
+        <div className="flex min-h-screen flex-col items-center p-8">
+            <h1 className="bold text-text text-4xl">{post.title}</h1>
+            <p className="font-accent mt-8">{post.description}</p>
+        </div>
+    );
 }
